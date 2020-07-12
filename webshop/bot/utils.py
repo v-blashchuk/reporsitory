5b9f587436da2c5e42e950725f6801245ce328ba
Melton <melton97zd@gmail.com>
2020-07-13 01:24:14 +0300

    utils

---
+++ webshop/bot/utils.py
@@ -0,0 +1,16 @@
+from .models import Text
+
+
+def init_text():
+    Text.objects.create(
+        title=Text.TITLES['greetings'],
+        body='Рады приветствовать Вас в нашем магазине!'
+    )
+
+    Text.objects.create(
+        title=Text.TITLES['cart'],
+        body='Вы перешли в корзину:'
+    )
+
+if __name__ == '__main__':
+    init_text()
